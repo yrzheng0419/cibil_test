d5adf2e89c872494b4a92fb7a464d973181df684
You-Rong (Eric) Zheng <ghhab852@gmail.com>
2026-06-13 22:49:16 +0800

    更新 site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://yrzheng0419.github.io/cibil_test/',   // ← 加這行
+  site: 'https://yrzheng0419.github.io',   // ← 加這行
   base: '/cibi_test',                     // ← 如果 repo 名稱不是根路徑才需要
   integrations: [tailwind()],
   output: 'static',
